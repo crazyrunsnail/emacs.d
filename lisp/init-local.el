d64aad6c315448571c57605163401686c107a8d8
fengjy <fjyuan1991@163.com>
2018-10-31 23:44:55 +0800

    add fontset and org-mode configuration but not working

---
+++ lisp/init-local.el
@@ -0,0 +1,26 @@
+;;; package --- customize usage
+;;; Commentary:
+;;; Code:
+
+;;; Set default font
+;;; (set-default-font  "-*-Menlo-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
+(set-default-font "Menlo-13")
+
+;;;
+(defun davino/org-mode()
+  "Customize org mode."
+  (progn)
+  (setq truncate-lines t)
+  (setq org-ellipsis "⤵")
+  )
+(add-hook 'org-mode 'davino/org-mode)
+
+
+;; (setq truncate-lines t)
+;; (setq org-ellipsis "⤵")
+
+;;; org-bullets
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(provide 'init-local)
