f4de5d6c1e9aed647d4cc767c71afe872cf41eeb
fengjy <fjyuan1991@163.com>
2018-10-31 23:43:37 +0800

    add elpa source of china emacs

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,13 @@
+;; -*- lexical-binding: t -*-
+;; set elpa source
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")
+                         ))
+
+
+(provide 'init-preload-local)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
