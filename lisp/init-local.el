51e63f9d203b973570e8229d10fe9534b280cd2a
crazyrunsnail <fjyuan1991@163.com>
2019-05-01 16:50:30 +0800

    Add local org config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,12 +4,15 @@
 
 ;;; Add local init config files
 (add-to-list 'load-path
-             (expand-file-name "local" (directory-file-name (file-name-directory load-file-name))))
+             (expand-file-name "local" (directory-file-name
+                                        (file-name-directory load-file-name))))
 
 
 (require 'better-default-local)
 (require 'org-local)
-
+(require 'evil-local)
 
 
 (provide 'init-local)
+
+;;; init-local.el ends here
--- lisp/local/org-local.el
+++ lisp/local/org-local.el
@@ -6,17 +6,39 @@
 ;;; ellipsis
 (setq org-ellipsis "⤵")
 
-;; truncate
-(setq truncate-lines t)
+;;;(setq org-startup-truncated nil)
 
+;;;(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 
 ;;; org-bullets
 (when (maybe-require-package 'org-bullets)
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-
 ;;; Personal schdule files
-(setq org-agenda-files (list "/Users/Rounds/Documents/MyNotes/MySchdules"))
+(setq org-agenda-files (list "/Users/Rounds/Documents/MySchdules"))
+
+;;; wrap code example
+(defun wrap-markup-region (start end)
+  "Insert `=' arounnd START or END region, backward if there no content between START and END."
+  (interactive "r")
+  (message "region start %d" (region-beginning))
+  (message "region end %d" (region-end))
+  (save-excursion
+    (goto-char end) (insert "= ")
+    (goto-char start) (insert " =")))
+
+;;; wrap the selection code
+(defun wrap-code-region (start end)
+  "START END."
+  (interactive "r")
+  (if (use-region-p)
+      (progn
+        (goto-char end) (insert "= ")
+        (goto-char start) (insert " ="))
+    (progn
+      (message "use-region-p is false")
+      (insert " =")
+      (insert "= "))))
 
 
 (provide 'org-local)
