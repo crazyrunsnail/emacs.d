1e9cfecaa9bcbde69842d130db066b4ee6b6d988
crazyrunsnail <fjyuan1991@163.com>
2019-05-01 16:51:35 +0800

    Local evil config(not used)

---
+++ lisp/local/evil-local.el
@@ -0,0 +1,12 @@
+;;; package --- local evil mode configuration
+;;; Commentary:
+;;; Code:
+
+
+;; (require-package 'evil)
+;; (require 'evil)
+;; (evil-mode t)
+
+(provide 'evil-local)
+
+;;; evil-local.el ends here
